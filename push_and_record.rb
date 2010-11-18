a33b29c6603c326057a6d349e040db08a00fc83e
zakuni <kunio038@gmail.com>
2010-11-18 17:38:38 +0900

    movファイルを変換する部分を追加、変換元は削除

--- push_and_record.rb
+++ push_and_record.rb
@@ -3,7 +3,11 @@ require 'rubygems'
 require 'appscript'
 require 'socket'
 require 'thread'
+require "ffmpeg"
+include FFMpeg
 include Appscript
+
+i = 0
 
 QP = app('QuickTime Player')
 
@@ -18,11 +22,21 @@ def rec_stop
   QP.documents[1].close()
 end
 
-def convert
- # オーディオ収録.movがあったら
-  #"なんとか.mp3"に変換する
-
- # オーディオ収録.movは削除する
+def convert_and_delete(file, i)
+  if i == 0
+    a = "a"
+    puts a
+  elsif i == 1
+    a = "b"
+    puts a
+  end
+  time = Time.now
+  sec = Time.now.sec
+  puts "time.now = #{time.sec}"
+  puts "sec = #{sec}"
+  convert "~/Movies/#{file}", :to => "~/Movies/#{a}.mp3" do end.run  #movをmp3に変換
+  puts "sec_after = #{sec}"
+#  `rm ~/Movies/オーディオ収録.mov` #オーディオ収録.movは削除する
 end
 
 sensors = Array.new(4, 980) # センサーの番号と値を入れる配列
@@ -43,9 +57,7 @@ t = Thread.new do
     
     puts "sensor0 #{sensors[0]}"
 
-    pressed = (sensors[1] < 600 || sensors[3] < 600)
-
-    if (sensors[0] > 950) #ドアが開いたら録音開始
+    if (sensors[0] < 950) #ドアが開いたら録音開始
       unless rec == 1
         rec = 1
         puts rec
@@ -56,7 +68,11 @@ t = Thread.new do
         rec = 0
         puts rec
         rec_stop
-        # convert
+        renamed = "#{Time.now.min}-#{Time.now.sec}"
+        File.rename(File.expand_path("~/Movies/オーディオ収録.mov"), File.expand_path("~/Movies/#{renamed}.mov"))
+        convert_path = (File.expand_path("~/Movies/#{renamed}"))
+        `ffmpeg -i "#{convert_path}.mov" "#{convert_path}.mp3"`
+        File.delete(File.expand_path("~/Movies/#{renamed}.mov")) # movファイルは消す
       end
     end
     sleep(0.1)
